68a10318bc7901ba647a67faf894a7d8e30c69a4
NetFlex <84316160+Geruays@users.noreply.github.com>
2021-11-24 15:34:14 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const fs = require('fs')
+const port = 80
+
+const server = http.createServer(function(req, res) {
+    res.writeHead(598,'Protected 24h')
+    fs.readFile('index.html', function(error, data){
+        if(error){
+            res.writeHead(598, 'Protected By NetFlex')
+            res.write('kebanyakan ddos tidak baik bagi kang coli')
+        }else {
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+server.listen(port, function(error){
+    if(error){
+        console.log('Protect By Tegar Gans' + error)
+    } else {
+        console.log('protect ' + port)
+    }   
+})
